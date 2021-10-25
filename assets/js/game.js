2a9bf534a4d9c1f656b151c281ba8fcc47129dfa
Vladimir Starchenko <vladimistar@gmail.com>
2021-10-24 20:25:32 -0400

    variables have been added and the console log has been introduced

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,14 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is you robots name?");
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+console.log(10 + 10);
+console.log("Our robots name is " + playerName)
+var name = "your name";
+console.log(name);
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+//fight();
