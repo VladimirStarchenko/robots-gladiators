c8a7c944db6e54d5b2c7abe2c7e9a53675372b47
Vladimir Starchenko <vladimistar@gmail.com>
2021-11-01 12:14:22 -0400

    prompt bug fixed

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,35 +4,64 @@ var randomNumber = function(min, max) {
 
     return value;
 };
+
+var fightOrSkip = function() {
+    
+    var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+    
+    if (promptFight === "" || promptFight === null) {
+        window.alert("You need to provide a valid answer! Please try again.");
+        
+        return fightOrSkip();
+    }
+    
+    promptFight = promptFight.toLowerCase();
+
+    if(promptFight === "skip") {
+        
+        var confirmSkip = window.confirm("Are you sure you'd like to quit?")
+
+if (confirmSkip) {
+    window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
+   
+    playerInfo.playerMoney = playerInfo.money -10;
+    shop();
+
+    return true;
+   }
+  }
+  return false;
+ };
+
+
 //fight function (now with parameter for enemy's name)
 var fight = function(enemy) {
-    while (playerInfo.health > 0 && enemy.health > 0) {
-        // ask player if they'd like to fight or run
-var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+    
+    var isPlayerTurn = true;
 
-
- // if player picks "skip" confirm and then stop the loop 
- if (promptFight === "skip" || promptFight === "SKIP") {
-    // confirm player want to skip
-    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-
-    //if yes (true), leave fight
-    if (confirmSkip) {
-        window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
-        // subtract money from the playerMoney for skipping
-        playerInfo.money = Math.max(0, playerInfo.money -10);
-        console.log("playerInfo.money", playerInfo.money);
-        break;
-      }
+    if (Math.random() > 0.5) {
+        isPlayerTurn = false;
     }
+    
+    while (playerInfo.health > 0 && enemy.health > 0) {
+        if (isPlayerTurn){
 
-    // remove enemy's health by subtracting the amount set in the playerAttack variable
-    var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
+        if(fightOrSkip()) {
+            break;
+          }
+          var damage = randomNumber(playerInfo.attack -3, playerInfo.attack);
 
-    enemy.health = Math.max(0, enemy.health - playerInfo.attack);
-    console.log(
-        playerInfo.Name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining."
-    );
+          enemy.health = Math.max(0, enemy.health - damage);
+          console.log(
+              playerInfo.name +
+              " attacked " +
+              enemy.name +
+              ". " +
+              enemy.name +
+              " now has " +
+              enemy.health +
+              " health remaining."
+          );
 
     // check enemy's health
     if (enemy.health <= 0) {
@@ -210,4 +239,4 @@ var endGame = function () {
             attack: randomNumber(10,14)
         }
       ];
-    startGame();
+    startGame();}
